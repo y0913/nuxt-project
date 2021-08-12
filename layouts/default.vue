009a21b8020811cf1a23635eb0464291a7158dc7
y0913 <yohei19910825@gmail.com>
2021-08-12 16:32:30 +0900

    update: スマホ向けheader, footerの呼び出し

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app dark>
-    <Header />
+    <HeaderPhone v-if="isSmartphone()" />
+    <Header v-else/>
     <v-main>
       <!-- <div style="margin: 0;"> -->
       <v-container>
@@ -8,18 +9,23 @@
       </v-container>
       <!-- </div> -->
     </v-main>
-    <Footer />
+    <FooterPhone v-if="isSmartphone()" />
+    <Footer v-else />
   </v-app>
 </template>
 
 <script>
 import Header from '~/components/Common/Header.vue'
+import HeaderPhone from '~/components/Common/HeaderPhone.vue'
 import Footer from '~/components/Common/Footer.vue'
+import FooterPhone from '~/components/Common/FooterPhone.vue'
 export default {
   name: 'App',
   components: {
    Header,
-   Footer
+   HeaderPhone,
+   Footer,
+   FooterPhone
   },
   data () {
     return {
@@ -30,6 +36,11 @@ export default {
       right: true,
       rightDrawer: false,
     }
+  },
+  methods: {
+    isSmartphone() {
+      return this.$ua.isFromSmartphone()
+      }
   }
 }
 </script>
