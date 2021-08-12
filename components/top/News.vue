7256a071c2f578bea67c639086e5976f94515cc7
y0913 <yohei19910825@gmail.com>
2021-08-13 01:40:55 +0900

    update: 不要な記述を削除

--- components/top/News.vue
+++ components/top/News.vue
@@ -17,7 +17,6 @@
         <span class="font-weight-light" style="color: white;">Twitter</span>
       </v-card-title>
       <v-card-text>
-        <!-- <a class="twitter-timeline" href="https://twitter.com/aza_engineer?ref_src=twsrc%5Etfw" data-height="600">Tweets by aza_engineer</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> -->
         <a class="twitter-timeline" data-height="600" href="https://twitter.com/fullcounts2015?ref_src=twsrc%5Etfw">Tweets by fullcounts2015</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </v-card-text>
     </v-card>
