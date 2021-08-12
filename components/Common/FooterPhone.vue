7dc84f670df7ef06c07497e4e2579b17d2161aba
y0913 <yohei19910825@gmail.com>
2021-08-12 16:32:04 +0900

    update:　スマホ向けheader, footer作成

---
+++ components/Common/FooterPhone.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-footer
+      :absolute="fixed"
+      app
+    >
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
+      </template>
+    </v-footer>
+</template>
+
+<script>
+
+export default {
+  name: 'FooterPhone',
+  data () {
+    return {
+      fixed: true,
+    }
+  }
+};
+</script>
---
+++ components/Common/HeaderPhone.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-container>
+    <v-app-bar
+      class="header"
+      app
+      flat
+      color="#FFFFFF"
+      dark
+    >
+      <v-app-bar-nav-icon
+        style="color: black;"
+        @click="drawer = true"
+        >
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title
+        style="color: black;"
+        class="font-weight-bold text-center"
+        >
+        {{title}}
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderPhone',
+  data () {
+    return {
+      menus:[
+        {
+          root:"about",
+          name:"About"
+        },
+        {
+          root:"players",
+          name:"Players"
+        },
+        {
+          root:"join",
+          name:"Join Team?"
+        },
+      ],
+      drawer: false,
+      group: null,
+      title: 'Kyoto Fullcounts',
+      root: '/'
+    }
+  }
+};
+</script>
